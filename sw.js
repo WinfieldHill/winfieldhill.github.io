e2b05391a4717806f815ee8d6d5e0e1b5035a2a9
WinfieldHill <winfieldhill@gmail.com>
2019-02-05 12:35:37 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 importScripts('https://winfieldhill.github.io/js/fetch_js_handler.js');
 
 const filesToCache = [
-   '/',
-   '/css/main.css',
-   '/js/fetch_js_handler.js',
-   '/js/app.js'
+  '/',
+  '/css/main.css',
+  '/js/fetch_js_handler.js',
+  '/js/app.js'
 ];
 
-const staticCacheName = 'pages-cache-v1';
+const staticCacheName = 'pages-cache-v2';
 
 self.addEventListener('install', event => {
   console.log('In sw.js file v5');
