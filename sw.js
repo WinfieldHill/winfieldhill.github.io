b4b102b0c95ab4a75b71338183db8b7cbb4d72df
WinfieldHill <winfieldhill@gmail.com>
2019-02-05 11:04:52 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const filesToCache = [
 const staticCacheName = 'pages-cache-v1';
 
 self.addEventListener('install', event => {
-	  console.log('Attempting to install service worker and cache static assets');
+	  console.log('Attempting to install service worker and cache static assets!');
 	  event.waitUntil(
 	    caches.open(staticCacheName)
 	    .then(cache => {
