277c91ad2dcd6c536deb0497fd9822b76d0f5278
WinfieldHill <winfieldhill@gmail.com>
2019-02-07 08:31:33 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const filesToCache = [
   '/js/app.js'
 ];
 
-const staticCacheName = 'pages-cache-v6';
+const staticCacheName = 'pages-cache-v7';
 
 self.addEventListener('install', event => { 
   self.skipWaiting();
